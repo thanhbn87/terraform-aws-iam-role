c3aa0db037adb547c4981309cb0ad6ebb49287e0
thanhbn <thanhbn87@gmail.com>
2019-09-06 14:20:28 +0700

    add outputs: id and arn

--- output.tf
+++ output.tf
@@ -7,3 +7,13 @@ output "name" {
   description = "The iam role name"
   value       = "${aws_iam_role.this.name}"
 }
+
+output "arn" {
+  description = "The iam role arn"
+  value       = "${aws_iam_role.this.arn}"
+}
+
+output "id" {
+  description = "The iam role id"
+  value       = "${aws_iam_role.this.id}"
+}
